41b188f08cd917759a77a7fe2cbf74e7ba31213d
bbagnes <bag18004@byui.edu>
2025-01-10 16:44:30 -0700

    Made changes to match directions and example website.

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+
+let currentDate = new Date();
+document.getElementById("currentyear").innerHTML = currentDate.getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
+
