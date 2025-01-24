daa12189059d9445b6283030b30ef8be58386a94
bbagnes <bag18004@byui.edu>
2025-01-23 17:52:06 -0700

    adjust for temp type

--- scripts/place.js
+++ scripts/place.js
@@ -8,9 +8,9 @@ document.getElementById("lastModified").innerHTML = "Last Modified: " + document
 // This is the wind Chill calculation: (35.74 + (temperature * 0.6215)) - (35.75 *(windspeed **.16)) + (0.4275 * temperature * (windspeed **0.16))
 
 let calculateWindChill = function(temperature, windspeed) {
-    let temperature = (temperature * 9/5) + 32;
-    let windChill = (35.74 + (temperature * 0.6215)) - (35.75 *(windspeed **.16)) + (0.4275 * temperature * (windspeed **0.16));
-    let windChillCelsius = Math.round(((windChill -32) * 5/9));
+    let fTemperature = (temperature * 9/5) + 32;
+    let windChill = (35.74 + (fTemperature * 0.6215)) - (35.75 *(windspeed **.16)) + (0.4275 * fTemperature * (windspeed **0.16));
+    let windChillCelsius = Math.round(((windChill -32) * 5/9)*10)/10;
     return windChillCelsius;
 }
 
